11fabf2c31460903391fdb2a0af3af7e7c81495c
randu0210 <scream0utgaming@gmail.com>
2020-07-20 17:30:10 +0700

    Add Get and Post method

---
+++ src/main/java/com/example/Employee_API/service/EmpService.java
@@ -0,0 +1,18 @@
+package com.example.Employee_API.service;
+
+import com.example.Employee_API.entity.Employee;
+import com.example.Employee_API.repository.EmpRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmpService {
+
+    @Autowired
+    private EmpRepository empRepository;
+
+    public List<Employee> getEmployee(){return empRepository.findAll();}
+
+}
